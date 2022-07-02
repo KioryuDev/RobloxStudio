bd50ef5f924abd4dfb4b41f15a08ac16a2c9c759
Dev: Kioryu <105140835+KioryuDev@users.noreply.github.com>
2022-07-02 14:10:18 +0900

    Update CameraControl.lua

--- CameraControl.lua
+++ CameraControl.lua
@@ -1,4 +1,6 @@
--- 게임메인화면의 흐름을 관리
+-- core gui 끄기
+local startGui = game:GetService("StarterGui")
+startGui:SetCoreGuiEnabled(Enum.CoreGuiType.All, false)
 
 -- 유저 캐릭터 제어권 제거
 local players = game.Players
@@ -15,6 +17,7 @@ local cameraFolder = workspace:WaitForChild("mCamera")
 local mc = cameraFolder.MainCamera
 
 local currentCamera = workspace.CurrentCamera
+local defaultSubject = currentCamera.CameraSubject
 local offset = CFrame.new(0,0,0) -- 1인칭
 
 currentCamera.CameraType = Enum.CameraType.Scriptable
@@ -22,23 +25,23 @@ currentCamera.CameraSubject = mc
 currentCamera.CFrame = mc.CFrame * offset
 
 -- 다른 카메라로 바로 변경
-wait(3)
+wait(2)
 local fc1 = cameraFolder.FaceCamera1
 currentCamera.CameraSubject = fc1
 currentCamera.CFrame = fc1.CFrame * offset
 
-wait(3)
+wait(2)
 local fc2 = cameraFolder.FaceCamera2
 currentCamera.CameraSubject = fc2
 currentCamera.CFrame = fc2.CFrame * offset
 
-wait(3)
+wait(2)
 local fc3 = cameraFolder.FaceCamera3
 currentCamera.CameraSubject = fc3
 currentCamera.CFrame = fc3.CFrame * offset
 
 -- weenservice를 이용한 이동
-wait(3)
+wait(2)
 currentCamera.CameraSubject = mc
 currentCamera.CFrame = mc.CFrame * offset
 
@@ -62,3 +65,8 @@ wait(3)
 local ts4 = tweenService:Create(currentCamera, TweenInfo.new(3), {CFrame = mc.CFrame})
 ts4:Play()
 ts4.Completed:Wait()
+
+wait(2)
+control:Enable()
+currentCamera.CameraSubject = defaultSubject
+currentCamera.CameraType = Enum.CameraType.Custom
