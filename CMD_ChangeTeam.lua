621165d66995dffeac732caa2a6644ac96d3d94d
Dev: Kioryu <105140835+KioryuDev@users.noreply.github.com>
2022-12-10 11:24:01 +0900

    Update CMD_ChangeTeam.lua
    운영자만 CMD 사용 가능하도록 변경

--- CMD_ChangeTeam.lua
+++ CMD_ChangeTeam.lua
@@ -1,22 +1,25 @@
 local cmd = "/changeTeam "
+local root = "kioryuDev"
 game.Players.PlayerAdded:Connect(function(player)
 	player.Chatted:Connect(function(message)
-		if string.sub(message, 1, #cmd) == cmd then
-			local msg = string.sub(message, #cmd + 1):split(" ")
-			local playerName = msg[1]
-			local teamName = msg[2]
-			
-			local player = game.Players:FindFirstChild(playerName)
-			if player then
-				local team = game.Teams:FindFirstChild(teamName)
-				if team then
-					player.Team = team	
+		if player.Name == root then
+			if string.sub(message, 1, #cmd) == cmd then
+				local msg = string.sub(message, #cmd + 1):split(" ")
+				local targetPlayerName = msg[1]
+				local teamName = msg[2]
+
+				local targetPlayer = game.Players:FindFirstChild(targetPlayerName)
+				if targetPlayer then
+					local team = game.Teams:FindFirstChild(teamName)
+					if team then
+						targetPlayer.Team = team	
+					else
+						warn(teamName, ": 해당 Team이 없습니다.")
+					end
 				else
-					warn(teamName, ": 해당 Team이 없습니다.")
+					warn(targetPlayerName, ": 해당 player가 없습니다.")
 				end
-			else
-				warn(playerName, ": 해당 player가 없습니다.")
-			end
+			end	
 		end
 	end)
 end)
