2d0e7e81f536eebca59362ea8cab8cdea8213e01
Dev: Kioryu <105140835+KioryuDev@users.noreply.github.com>
2023-08-23 17:04:56 +0900

    Create RemoveAccessories.lua

---
+++ RemoveAccessories.lua
@@ -0,0 +1,12 @@
+local parent = script.Parent
+
+parent.Touched:Connect(function(otherPart)
+	local char = otherPart.Parent
+	if game.Players:GetPlayerFromCharacter(char) then
+		local huma = char:FindFirstChildOfClass("Humanoid")
+		if huma and huma:GetState() ~= Enum.HumanoidStateType.Dead and #huma:GetAccessories() > 0 then
+			huma:RemoveAccessories()
+			print(1)
+		end
+	end 
+end)
